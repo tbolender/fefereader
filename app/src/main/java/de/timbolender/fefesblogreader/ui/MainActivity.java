240e5476652a0a32202af76f5d3804cd54472645
Tim Bolender <git@timbolender.de>
2017-02-02 23:32:43 +0100

    Initial project.

---
+++ app/src/main/java/de/timbolender/fefesblogreader/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package de.timbolender.fefesblogreader.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.timbolender.fefesblogreader.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
