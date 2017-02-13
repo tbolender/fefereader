929a26e3e7757a216292a53211a045768f334c10
Tim Bolender <git@timbolender.de>
2017-02-14 00:16:08 +0100

    Created util class for HTML parsing.

--- app/src/main/java/de/timbolender/fefesblogreader/ui/view/PostListItem.java
+++ app/src/main/java/de/timbolender/fefesblogreader/ui/view/PostListItem.java
@@ -2,8 +2,6 @@ package de.timbolender.fefesblogreader.ui.view;
 
 import android.content.Context;
 import android.graphics.Typeface;
-import android.os.Build;
-import android.text.Html;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -11,6 +9,7 @@ import android.widget.TextView;
 import butterknife.ButterKnife;
 import de.timbolender.fefesblogreader.R;
 import de.timbolender.fefesblogreader.data.Post;
+import de.timbolender.fefesblogreader.util.Html;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -40,14 +39,7 @@ public class PostListItem extends LinearLayout {
 
         // Set text values
         TextView preview = ButterKnife.findById(this, R.id.contents_preview);
-        String previewText;
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-            //noinspection deprecation
-            previewText = Html.fromHtml(post.getContents()).toString();
-        }
-        else {
-            previewText = Html.fromHtml(post.getContents(), Html.FROM_HTML_MODE_COMPACT).toString();
-        }
+        String previewText = Html.fromHtml(post.getContents()).toString();
         preview.setText(previewText);
         preview.setTypeface((post.isRead() && !post.isUpdated()) ? Typeface.DEFAULT : Typeface.DEFAULT_BOLD);
 
