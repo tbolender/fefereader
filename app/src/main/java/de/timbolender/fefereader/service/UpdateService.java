0102ad3b8e682c945fd10180088c1c4e6376ac78
Tim Bolender <git@timbolender.de>
2017-02-14 18:10:49 +0100

    Moved updating to background service.

---
+++ app/src/main/java/de/timbolender/fefereader/service/UpdateService.java
@@ -0,0 +1,121 @@
+package de.timbolender.fefereader.service;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+
+import de.timbolender.fefereader.data.RawPost;
+import de.timbolender.fefereader.db.DatabaseWrapper;
+import de.timbolender.fefereader.db.SQLiteOpenHelper;
+import de.timbolender.fefereader.db.SQLiteWrapper;
+import de.timbolender.fefereader.network.Fetcher;
+import de.timbolender.fefereader.network.Parser;
+import okhttp3.OkHttpClient;
+
+/**
+ * Perform regular post updates in the background.
+ */
+public class UpdateService extends Service {
+    static final String TAG = UpdateService.class.getSimpleName();
+    static final String ACTION_UPDATE = "de.timbolender.fefereader.service.action.UPDATE";
+
+    public static final String BROADCAST_UPDATE_FINISHED = "de.timbolender.fefereader.service.action.UPDATE_FINISHED";
+    public static final int BROADCAST_PRIORITY_UI = 10;
+    public static final int BROADCAST_PRIORITY_SERVICE = 0;
+
+    /**
+     * Trigger update in background service
+     * @param context Context to use.
+     */
+    public static void startUpdate(Context context) {
+        Intent updateIntent = new Intent(context, UpdateService.class);
+        updateIntent.setAction(ACTION_UPDATE);
+        context.startService(updateIntent);
+    }
+
+    DatabaseWrapper databaseWrapper;
+    BroadcastReceiver updateReceiver;
+
+    @Override
+    public void onCreate() {
+        // Get access to database
+        SQLiteOpenHelper databaseHelper = new SQLiteOpenHelper(this);
+        SQLiteDatabase database = databaseHelper.getWritableDatabase();
+        databaseWrapper = new SQLiteWrapper(database);
+
+        // Register broadcast receiver for notifications
+        updateReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // TODO: Show notification about update
+            }
+        };
+        IntentFilter intentFilter = new IntentFilter(BROADCAST_UPDATE_FINISHED);
+        intentFilter.setPriority(BROADCAST_PRIORITY_SERVICE);
+        registerReceiver(updateReceiver, intentFilter);
+
+        // TODO: Register normal regular update behavior
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Trigger update
+        if(intent.getAction().equals(ACTION_UPDATE)) {
+            Log.i(TAG, "Starting post update.");
+            // TODO: Check whether update is running
+            performUpdate();
+        }
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(updateReceiver);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        // We do not support binding
+        return null;
+    }
+
+    /**
+     * Start update in a separate thread.
+     */
+    private void performUpdate() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Parser parser = new Parser();
+                    Fetcher fetcher = new Fetcher(client, parser);
+                    List<RawPost> posts = fetcher.fetch();
+
+                    for(RawPost post : posts) {
+                        databaseWrapper.addOrUpdatePost(post);
+                    }
+
+                    Intent finishedIntent = new Intent(BROADCAST_UPDATE_FINISHED);
+                    sendBroadcast(finishedIntent);
+                }
+                catch(ParseException | IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+}
--- app/src/main/java/de/timbolender/fefereader/ui/MainActivity.java
+++ app/src/main/java/de/timbolender/fefereader/ui/MainActivity.java
@@ -1,9 +1,11 @@
 package de.timbolender.fefereader.ui;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,22 +15,15 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.List;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.timbolender.fefereader.R;
 import de.timbolender.fefereader.data.Post;
-import de.timbolender.fefereader.data.RawPost;
 import de.timbolender.fefereader.db.DatabaseWrapper;
 import de.timbolender.fefereader.db.PostReader;
 import de.timbolender.fefereader.db.SQLiteOpenHelper;
 import de.timbolender.fefereader.db.SQLiteWrapper;
-import de.timbolender.fefereader.network.Fetcher;
-import de.timbolender.fefereader.network.Parser;
-import okhttp3.OkHttpClient;
+import de.timbolender.fefereader.service.UpdateService;
 
 public class MainActivity extends AppCompatActivity implements PostAdapter.OnPostSelectedListener {
     SQLiteOpenHelper databaseHelper;
@@ -36,6 +31,8 @@ public class MainActivity extends AppCompatActivity implements PostAdapter.OnPos
     DatabaseWrapper databaseWrapper;
 
     @BindView(R.id.post_list) RecyclerView postList;
+
+    BroadcastReceiver updateReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,9 +55,15 @@ public class MainActivity extends AppCompatActivity implements PostAdapter.OnPos
         postAdapter = new PostAdapter(reader, this);
         postList.setAdapter(postAdapter);
 
-        // Just for testing, allow retrieval in main thread
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        // Create receiver for updates
+        updateReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                updateAdapter();
+                Toast.makeText(MainActivity.this, "Feed aktualisiert", Toast.LENGTH_SHORT).show();
+                abortBroadcast();
+            }
+        };
     }
 
     @Override
@@ -73,7 +76,7 @@ public class MainActivity extends AppCompatActivity implements PostAdapter.OnPos
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.menu_refresh) {
-            onRefreshClick();
+            UpdateService.startUpdate(this);
             return true;
         }
         else if(item.getItemId() == R.id.menu_mark_read) {
@@ -86,29 +89,27 @@ public class MainActivity extends AppCompatActivity implements PostAdapter.OnPos
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Register broadcast receiver for notifications
+        IntentFilter intentFilter = new IntentFilter(UpdateService.BROADCAST_UPDATE_FINISHED);
+        intentFilter.setPriority(UpdateService.BROADCAST_PRIORITY_UI);
+        registerReceiver(updateReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(updateReceiver);
+
+        super.onPause();
+    }
+
+    @Override
     protected void onDestroy() {
         databaseWrapper.cleanUp();
         databaseHelper.close();
         super.onDestroy();
-    }
-
-    public void onRefreshClick() {
-        try {
-            OkHttpClient client = new OkHttpClient();
-            Parser parser = new Parser();
-            Fetcher fetcher = new Fetcher(client, parser);
-            List<RawPost> posts = fetcher.fetch();
-
-            for(RawPost post : posts) {
-                databaseWrapper.addOrUpdatePost(post);
-            }
-
-            updateAdapter();
-            Toast.makeText(this, "Feed aktualisiert", Toast.LENGTH_SHORT).show();
-        }
-        catch(ParseException | IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public void onMarkAllAsReadClick() {
