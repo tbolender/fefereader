8549d19bf75c9728626fb81170bab19043cd436c
Tim Bolender <git@timbolender.de>
2019-01-11 16:49:43 +0100

    Drop Post Parceable implementation

--- app/src/main/java/de/timbolender/fefereader/data/Post.kt
+++ app/src/main/java/de/timbolender/fefereader/data/Post.kt
@@ -1,8 +1,8 @@
 package de.timbolender.fefereader.data
 
-import android.os.Parcel
-import android.os.Parcelable
-
+/**
+ * A single blog post.
+ */
 data class Post(
         val id: String,
         val timestampId: Long,
@@ -11,40 +11,4 @@ data class Post(
         val isBookmarked: Boolean,
         val contents: String,
         val date: Long
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readLong(),
-        parcel.readByte() != 0.toByte(),
-        parcel.readByte() != 0.toByte(),
-        parcel.readByte() != 0.toByte(),
-        parcel.readString(),
-        parcel.readLong()) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(id)
-        parcel.writeLong(timestampId)
-        parcel.writeByte(if (isRead) 1 else 0)
-        parcel.writeByte(if (isUpdated) 1 else 0)
-        parcel.writeByte(if (isBookmarked) 1 else 0)
-        parcel.writeString(contents)
-        parcel.writeLong(date)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Post> {
-        override fun createFromParcel(parcel: Parcel): Post {
-            return Post(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Post?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
+);
