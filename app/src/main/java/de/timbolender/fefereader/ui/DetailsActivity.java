40f933c588b1fe8fe2df5e248204fdec3a7333ad
Tim Bolender <git@timbolender.de>
2019-01-11 16:41:04 +0100

    Introduce live data for details activity

--- app/src/main/java/de/timbolender/fefereader/ui/DetailsActivity.java
+++ app/src/main/java/de/timbolender/fefereader/ui/DetailsActivity.java
@@ -20,6 +20,7 @@ import java.util.Date;
 import java.util.Locale;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.timbolender.fefereader.R;
@@ -44,7 +45,7 @@ public class DetailsActivity extends AppCompatActivity implements PostView.OnLin
     DatabaseWrapper databaseWrapper;
     BroadcastReceiver updateReceiver;
     PreferenceHelper preferenceHelper;
-    Post post;
+    MutableLiveData<Post> postData;
     @BindView(R.id.post_view)
     PostView postView;
 
@@ -74,18 +75,21 @@ public class DetailsActivity extends AppCompatActivity implements PostView.OnLin
         SQLiteDatabase database = databaseHelper.getWritableDatabase();
         databaseWrapper = new SQLiteWrapper(database);
 
-        // Extract post
-        final Intent intent = getIntent();
-        post = intent.getParcelableExtra(INTENT_EXTRA_POST);
+        // Track post changes
+        postData = new MutableLiveData<>();
+        postData.observe(this, post -> {
+            // Set title
+            Date data = new Date(post.getDate());
+            setTitle(DATE_FORMAT.format(data));
 
-        // Set title
-        Date data = new Date(post.getDate());
-        setTitle(DATE_FORMAT.format(data));
+            // Fill view
+            String customStyle = preferenceHelper.getPostStyle();
+            postView.fill(post, customStyle);
+            postView.setOnLinkClickedListener(this);
 
-        // Fill view
-        String customStyle = preferenceHelper.getPostStyle();
-        postView.fill(post, customStyle);
-        postView.setOnLinkClickedListener(this);
+            // Update menu icons
+            invalidateOptionsMenu();
+        });
 
         // Create receiver for updates
         updateReceiver = new BroadcastReceiver() {
@@ -95,6 +99,10 @@ public class DetailsActivity extends AppCompatActivity implements PostView.OnLin
                 abortBroadcast();
             }
         };
+
+        // Extract post
+        final Intent intent = getIntent();
+        postData.postValue(intent.getParcelableExtra(INTENT_EXTRA_POST));
     }
 
     @Override
@@ -102,24 +110,31 @@ public class DetailsActivity extends AppCompatActivity implements PostView.OnLin
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_details, menu);
 
-        MenuItem item = menu.findItem(R.id.menu_bookmarked);
-        item.setTitle(post.isBookmarked() ? R.string.menu_bookmark_delete : R.string.menu_bookmark_create);
-        item.setIcon(post.isBookmarked() ? R.drawable.ic_bookmark_border : R.drawable.ic_bookmark);
+        Post post = postData.getValue();
+        if(post != null) {
+            MenuItem item = menu.findItem(R.id.menu_bookmarked);
+            item.setTitle(post.isBookmarked() ? R.string.menu_bookmark_delete : R.string.menu_bookmark_create);
+            item.setIcon(post.isBookmarked() ? R.drawable.ic_bookmark_border : R.drawable.ic_bookmark);
+        }
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Ensure post is present
+        Post currentPost = postData.getValue();
+        if(currentPost == null)
+            return super.onOptionsItemSelected(item);
+
         if(item.getItemId() == R.id.menu_bookmarked) {
-            databaseWrapper.setBookmarked(post.getId(), !post.isBookmarked());
-            post = databaseWrapper.getPost(post.getId());
-            invalidateOptionsMenu();
+            databaseWrapper.setBookmarked(currentPost.getId(), !currentPost.isBookmarked());
+            postData.postValue(databaseWrapper.getPost(currentPost.getId()));
             return true;
         }
         else if(item.getItemId() == R.id.menu_share) {
             Intent shareIntent = new Intent();
             shareIntent.setAction(Intent.ACTION_SEND);
-            shareIntent.putExtra(Intent.EXTRA_TEXT, String.format(SHARE_URL, post.getId()));
+            shareIntent.putExtra(Intent.EXTRA_TEXT, String.format(SHARE_URL, currentPost.getId()));
             shareIntent.setType("text/plain");
 
             startActivity(Intent.createChooser(shareIntent, getString(R.string.intent_share_title)));
