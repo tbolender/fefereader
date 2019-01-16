6e6b6be68bcf633252a1d2b0a3f583e4cbc8fe98
Tim Bolender <git@timbolender.de>
2019-01-16 19:12:15 +0100

    Use singleton for binding converters

--- app/src/main/java/de/timbolender/fefereader/util/BindingConverters.kt
+++ app/src/main/java/de/timbolender/fefereader/util/BindingConverters.kt
@@ -5,21 +5,23 @@ import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
-@BindingAdapter("android:visibility")
-fun setVisibility(view: View, visible: Boolean) {
-    view.visibility = if (visible) View.VISIBLE else View.GONE
-}
+object BindingConverters {
+    @BindingAdapter("android:visibility")
+    fun setVisibility(view: View, visible: Boolean) {
+        view.visibility = if (visible) View.VISIBLE else View.GONE
+    }
 
-@BindingAdapter("android:html_text")
-fun setHtmlText(view: TextView, value: String?) {
-    view.text = Html.fromHtml(if (value != null) value else "").toString()
-}
+    @BindingAdapter("android:html_text")
+    fun setHtmlText(view: TextView, value: String?) {
+        view.text = Html.fromHtml(if (value != null) value else "").toString()
+    }
 
-@BindingAdapter("android:textStyle")
-fun setTextStyle(view: TextView, value: String?) {
-    when(value) {
-        "bold" -> view.setTypeface(view.typeface, Typeface.BOLD)
-        "italic" -> view.setTypeface(view.typeface, Typeface.ITALIC)
-        else -> view.setTypeface(view.typeface, Typeface.NORMAL)
+    @BindingAdapter("android:textStyle")
+    fun setTextStyle(view: TextView, value: String?) {
+        when (value) {
+            "bold" -> view.setTypeface(view.typeface, Typeface.BOLD)
+            "italic" -> view.setTypeface(view.typeface, Typeface.ITALIC)
+            else -> view.setTypeface(view.typeface, Typeface.NORMAL)
+        }
     }
 }
