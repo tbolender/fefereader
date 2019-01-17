322a4f5fd636b2a0328d97bbfba1114a6ebae6ad
Tim Bolender <git@timbolender.de>
2019-01-17 02:03:13 +0100

    Fix binding converters issue

--- app/src/main/java/de/timbolender/fefereader/util/BindingConverters.kt
+++ app/src/main/java/de/timbolender/fefereader/util/BindingConverters.kt
@@ -6,16 +6,19 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
 object BindingConverters {
+    @JvmStatic
     @BindingAdapter("android:visibility")
     fun setVisibility(view: View, visible: Boolean) {
         view.visibility = if (visible) View.VISIBLE else View.GONE
     }
 
+    @JvmStatic
     @BindingAdapter("android:html_text")
     fun setHtmlText(view: TextView, value: String?) {
         view.text = Html.fromHtml(if (value != null) value else "").toString()
     }
 
+    @JvmStatic
     @BindingAdapter("android:textStyle")
     fun setTextStyle(view: TextView, value: String?) {
         when (value) {
