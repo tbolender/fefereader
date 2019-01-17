12e096d6ad7d59baa9c39577f4a0b0f79e2104ce
Tim Bolender <git@timbolender.de>
2019-01-17 01:37:16 +0100

    Add database initialization code

--- app/src/main/java/de/timbolender/fefereader/db/AppDatabase.kt
+++ app/src/main/java/de/timbolender/fefereader/db/AppDatabase.kt
@@ -1,11 +1,28 @@
 package de.timbolender.fefereader.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(version = 1, entities = [Post::class])
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            if(INSTANCE == null) {
+                synchronized(AppDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "fefe-news")
+                            .build()
+                }
+            }
+
+            return INSTANCE!!
+        }
+    }
+
     abstract fun postDao(): PostDao
 }
