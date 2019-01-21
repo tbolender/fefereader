38021f5d7d6092cfea4b4de6e96a66a7ad138b12
Tim Bolender <git@timbolender.de>
2019-01-21 15:01:55 +0100

    Use Kotlin style to define simple passing methods

--- app/src/main/java/de/timbolender/fefereader/db/DataRepository.kt
+++ app/src/main/java/de/timbolender/fefereader/db/DataRepository.kt
@@ -1,7 +1,6 @@
 package de.timbolender.fefereader.db
 
 import android.app.Application
-import androidx.lifecycle.LiveData
 
 /**
  * Central data access class. Automatically triggers updates if required.
@@ -10,15 +9,9 @@ class DataRepository(application: Application) {
     private val db = AppDatabase.getInstance(application)
     private val postDao = db.postDao()
 
-    fun createOrUpdatePostSync(post: Post): Long {
-        return postDao.insertPosts(post)[0]
-    }
+    fun createOrUpdatePostSync(post: Post) = postDao.insertPosts(post)[0]
 
-    fun getPost(postId: String): LiveData<Post?> {
-        return postDao.loadPost(postId)
-    }
+    fun getPost(postId: String) = postDao.loadPost(postId)
 
-    fun getPostSync(postId: String): Post? {
-        return postDao.loadPostSync(postId)
-    }
+    fun getPostSync(postId: String) = postDao.loadPostSync(postId)
 }
