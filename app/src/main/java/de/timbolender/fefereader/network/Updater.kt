3f507f1fa9ebb86a5ea524fe6644aa24028e0a77
Tim Bolender <git@timbolender.de>
2019-01-21 11:36:12 +0100

    Complete updater

--- app/src/main/java/de/timbolender/fefereader/network/Updater.kt
+++ app/src/main/java/de/timbolender/fefereader/network/Updater.kt
@@ -1,5 +1,6 @@
 package de.timbolender.fefereader.network
 
+import android.util.Log
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import de.timbolender.fefereader.db.DataRepository
 import de.timbolender.fefereader.db.Post
@@ -9,6 +10,10 @@ import java.text.ParseException
 import java.util.*
 
 class Updater(private val repository: DataRepository) {
+    companion object {
+        private var TAG: String = Updater::class.simpleName!!
+    }
+
     @Throws(ParseException::class, IOException::class)
     fun update() {
         val client = OkHttpClient.Builder()
@@ -20,10 +25,16 @@ class Updater(private val repository: DataRepository) {
         val posts = fetcher.fetch()
 
         for (post in posts) {
-            val dbPost = Post(
-                    0, post.timestampId, false, false, false,
-                    post.contents, Date(post.date))
-            repository.createPost(dbPost)
+            val existingPost = repository.getPostSync(post.id)
+            val dbPost = if(existingPost == null) {
+                Log.d(TAG, "Inserted new post ${post.id}")
+                Post(post.id, post.timestampId, false, false, false, post.contents, Date(post.date))
+            }
+            else {
+                Log.d(TAG, "Updated post ${post.id}")
+                existingPost.copy(contents = post.contents, isUpdated = true)
+            }
+            repository.createOrUpdatePostSync(dbPost)
         }
     }
 }
