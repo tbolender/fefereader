f2671506b47de85bdaf3b6bb8f27026fae0873d9
Tim Bolender <git@timbolender.de>
2019-01-22 01:12:27 +0100

    Dropping unnecessary database code

--- app/src/main/java/de/timbolender/fefereader/db/DatabaseWrapper.java
+++ app/src/main/java/de/timbolender/fefereader/db/DatabaseWrapper.java
@@ -1,7 +1,5 @@
 package de.timbolender.fefereader.db;
 
-import de.timbolender.fefereader.data.Post;
-
 /**
  * Wrapper interface around a data source.
  */
@@ -9,14 +7,6 @@ public interface DatabaseWrapper {
     int FILTER_NONE = 1;
     int FILTER_BOOKMARKED = 1 << 1;
     int FILTER_UNREAD = 1 << 2;
-
-    /**
-     * Fetch post with given id.
-     * @param id Id of post.
-     * @return The post as it is stored in the database.
-     * @throws DatabaseException
-     */
-    Post getPost(String id) throws DatabaseException;
 
     /**
      * @return Number of posts which are unread.
@@ -27,32 +17,4 @@ public interface DatabaseWrapper {
      * @return Number of posts which are updated and were already read in the past.
      */
     long getUpdatedPostCount();
-
-    /**
-     * @return Reader providing access to all posts.
-     * @param filter Filter expression which posts to return.
-     * @throws DatabaseException Thrown on database error.
-     */
-    PostReader getPostsReader(int filter) throws DatabaseException;
-
-    /**
-     * Save whether post is marked as read. Removes the updated flag (independent of parameter).
-     * @param id Post to mark as read.
-     * @param isRead True if post was read.
-     * @throws DatabaseException Thrown on database error.
-     */
-    void setRead(String id, boolean isRead) throws DatabaseException;
-
-    /**
-     * Save whether post is saved for later.
-     * @param id Id of post.
-     * @param isBookmarked True if post should be saved as bookmark.
-     * @throws DatabaseException Thrown on database error.
-     */
-    void setBookmarked(String id, boolean isBookmarked) throws DatabaseException;
-
-    /**
-     * Clear everything up.
-     */
-    void cleanUp();
 }
--- app/src/main/java/de/timbolender/fefereader/db/SQLiteWrapper.java
+++ app/src/main/java/de/timbolender/fefereader/db/SQLiteWrapper.java
@@ -1,13 +1,8 @@
 package de.timbolender.fefereader.db;
 
-import android.annotation.SuppressLint;
-import android.content.ContentValues;
-import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-
-import de.timbolender.fefereader.data.Post;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static de.timbolender.fefereader.db.SQLiteFefesBlogContract.PostEntry;
@@ -24,49 +19,6 @@ public class SQLiteWrapper implements DatabaseWrapper {
         checkNotNull(database);
 
         this.database = database;
-    }
-
-    @Override
-    public Post getPost(String id) throws DatabaseException {
-        try {
-            checkNotNull(id);
-
-            String[] projection = {
-                PostEntry._ID,
-                PostEntry.COLUMN_NAME_TIMESTAMP_ID,
-                PostEntry.COLUMN_NAME_IS_READ,
-                PostEntry.COLUMN_NAME_IS_UPDATED,
-                PostEntry.COLUMN_NAME_IS_BOOKMARKED,
-                PostEntry.COLUMN_NAME_CONTENTS,
-                PostEntry.COLUMN_NAME_DATE
-            };
-
-            String selection = PostEntry._ID + " = ?";
-            String[] selectionArgs = {id};
-
-            Cursor cursor = database.query(PostEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, null);
-            if(!cursor.moveToNext()) {
-                cursor.close();
-                throw new DatabaseException("No post found with id " + id);
-            }
-
-            Post post = new Post(
-                cursor.getString(cursor.getColumnIndexOrThrow(PostEntry._ID)),
-                Long.parseLong(cursor.getString(cursor.getColumnIndexOrThrow(PostEntry.COLUMN_NAME_TIMESTAMP_ID))),
-                cursor.getInt(cursor.getColumnIndexOrThrow(PostEntry.COLUMN_NAME_IS_READ)) == 1,
-                cursor.getInt(cursor.getColumnIndexOrThrow(PostEntry.COLUMN_NAME_IS_UPDATED)) == 1,
-                cursor.getInt(cursor.getColumnIndexOrThrow(PostEntry.COLUMN_NAME_IS_BOOKMARKED)) == 1,
-                cursor.getString(cursor.getColumnIndexOrThrow(PostEntry.COLUMN_NAME_CONTENTS)),
-                Long.parseLong(cursor.getString(cursor.getColumnIndexOrThrow(PostEntry.COLUMN_NAME_DATE)))
-            );
-
-            cursor.close();
-
-            return post;
-        }
-        catch(SQLException e) {
-            throw new DatabaseException(e);
-        }
     }
 
     @Override
@@ -89,84 +41,5 @@ public class SQLiteWrapper implements DatabaseWrapper {
         catch(SQLException e) {
             throw new DatabaseException(e);
         }
-    }
-
-    @Override
-    public PostReader getPostsReader(int filter) throws DatabaseException {
-        // Make sure no parameter misuse is done
-        int filterCount = 0;
-        filterCount += (filter & FILTER_NONE) != 0 ? 1 : 0;
-        filterCount += (filter & FILTER_BOOKMARKED) != 0 ? 1 : 0;
-        filterCount += (filter & FILTER_UNREAD) != 0 ? 1 : 0;
-        if(filterCount != 1) {
-            throw new IllegalArgumentException("Passed illegal filter flag combination");
-        }
-
-        try {
-            String[] projection = {
-                PostEntry._ID,
-                PostEntry.COLUMN_NAME_TIMESTAMP_ID,
-                PostEntry.COLUMN_NAME_IS_READ,
-                PostEntry.COLUMN_NAME_IS_UPDATED,
-                PostEntry.COLUMN_NAME_IS_BOOKMARKED,
-                PostEntry.COLUMN_NAME_CONTENTS,
-                PostEntry.COLUMN_NAME_DATE
-            };
-
-            String sortOrder = PostEntry.COLUMN_NAME_TIMESTAMP_ID + " DESC";
-
-            // Respect filtering
-            String selection = null;
-            if(filter == FILTER_BOOKMARKED) {
-                selection = PostEntry.COLUMN_NAME_IS_BOOKMARKED + " = 1";
-            }
-            if(filter == FILTER_UNREAD) {
-                selection = PostEntry.COLUMN_NAME_IS_READ + " = 0 OR " + PostEntry.COLUMN_NAME_IS_UPDATED + " = 1";
-            }
-
-            @SuppressLint("Recycle")
-            Cursor cursor = database.query(PostEntry.TABLE_NAME, projection, selection, null, null, null, sortOrder);
-
-            return new SQLiteReader(cursor);
-        }
-        catch(SQLException e) {
-            throw new DatabaseException(e);
-        }
-    }
-
-    @Override
-    public void setRead(String id, boolean isRead) throws DatabaseException {
-        checkNotNull(id);
-
-        ContentValues values = new ContentValues();
-        values.put(PostEntry.COLUMN_NAME_IS_READ, isRead);
-        values.put(PostEntry.COLUMN_NAME_IS_UPDATED, false);
-
-        String selection = PostEntry._ID + " = ?";
-        String[] selectionArgs = { id };
-
-        if(database.update(PostEntry.TABLE_NAME, values, selection, selectionArgs) == 0) {
-            throw new DatabaseException("No post found with id " + id);
-        }
-    }
-
-    @Override
-    public void setBookmarked(String id, boolean isBookmarked) throws DatabaseException {
-        checkNotNull(id);
-
-        ContentValues values = new ContentValues();
-        values.put(PostEntry.COLUMN_NAME_IS_BOOKMARKED, isBookmarked);
-
-        String selection = PostEntry._ID + " = ?";
-        String[] selectionArgs = { id };
-
-        if(database.update(PostEntry.TABLE_NAME, values, selection, selectionArgs) == 0) {
-            throw new DatabaseException("No post found with id " + id);
-        }
-    }
-
-    @Override
-    public void cleanUp() {
-        database.close();
     }
 }
