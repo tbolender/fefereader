eee65ac36339a0e27aa0c8382f5e4099de36bd32
Tim Bolender <git@timbolender.de>
2019-02-06 00:14:46 +0100

    Create migration for Room

--- app/src/main/java/de/timbolender/fefereader/db/AppDatabase.kt
+++ app/src/main/java/de/timbolender/fefereader/db/AppDatabase.kt
@@ -5,6 +5,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
+import de.timbolender.fefereader.db.migration.ManualToRoomMigration
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Database(version = 1, entities = [Post::class])
 @TypeConverters(Converters::class)
@@ -16,12 +21,30 @@ abstract class AppDatabase : RoomDatabase() {
         fun getInstance(context: Context): AppDatabase {
             if(INSTANCE == null) {
                 synchronized(AppDatabase::class) {
-                    INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "fefe-news")
-                            .build()
+                    INSTANCE = buildDatabase(context)
                 }
             }
 
             return INSTANCE!!
+        }
+
+        private fun buildDatabase(context: Context): AppDatabase {
+            return Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "fefe-news")
+                .addCallback(object: Callback() {
+                    override fun onCreate(db: SupportSQLiteDatabase) {
+                        super.onCreate(db)
+
+                        // Migrate from old manual database version
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val postDao = getInstance(context).postDao()
+                            val initialMigration = ManualToRoomMigration(context, postDao)
+                            if(initialMigration.isMigrationNecessary) {
+                                initialMigration.migrate()
+                            }
+                        }
+                    }
+                })
+                .build()
         }
     }
 
