45896c6ceb53d2b32efed1ded69c60b716696a39
Tim Bolender <git@timbolender.de>
2019-02-11 11:07:26 +0100

    Prevent processing of spoofed broadcast

--- app/src/main/java/de/timbolender/fefereader/service/BootServiceStarter.java
+++ app/src/main/java/de/timbolender/fefereader/service/BootServiceStarter.java
@@ -4,12 +4,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import java.util.Objects;
+
 /**
  * Broadcast receiver starting the update service. Intended for automatic launching of service after boot.
  */
 public class BootServiceStarter extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        String action = Objects.requireNonNull(intent.getAction());
+        if(!action.equals("android.intent.action.BOOT_COMPLETED") &&
+           !action.equals("android.intent.action.MY_PACKAGE_REPLACED"))
+            return;
+
         UpdateService.startManualUpdate(context);
     }
 }
