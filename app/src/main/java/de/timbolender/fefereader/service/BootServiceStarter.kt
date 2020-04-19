ef1c60a3104992c27d298a57c246af88647caa41
Tim Bolender <git@timbolender.de>
2020-04-19 12:18:23 +0200

    Reuse service to provide long running context

--- app/src/main/java/de/timbolender/fefereader/service/BootServiceStarter.kt
+++ app/src/main/java/de/timbolender/fefereader/service/BootServiceStarter.kt
@@ -3,11 +3,10 @@ package de.timbolender.fefereader.service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import de.timbolender.fefereader.db.DataRepository
 import de.timbolender.fefereader.util.PreferenceHelper
 
 /**
- * Broadcast receiver starting the update service. Intended for automatic launching of service after boot.
+ * Broadcast receiver starting the notification service. Sets up automatic updates as well.
  */
 class BootServiceStarter : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
@@ -15,11 +14,10 @@ class BootServiceStarter : BroadcastReceiver() {
         if (action != "android.intent.action.BOOT_COMPLETED" && action != "android.intent.action.MY_PACKAGE_REPLACED")
             return
 
-        val repository = DataRepository(context)
-        val notificationCreator = NotificationReceiver(repository)
-        notificationCreator.register(context)
-
         val preferenceHelper = PreferenceHelper(context)
         UpdateWorker.configureAutomaticUpdates(context, preferenceHelper)
+
+        val serviceIntent = Intent(context.applicationContext, NotificationService::class.java)
+        context.applicationContext.startService(serviceIntent)
     }
 }
