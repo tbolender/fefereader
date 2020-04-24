8cf4373783f4c8ed9a2e3b60d96239e69c636fe3
Tim Bolender <git@timbolender.de>
2020-04-24 22:19:55 +0200

    Remove unnecessary crash configuration

--- app/src/main/java/de/timbolender/fefereader/FefeReaderApplication.kt
+++ app/src/main/java/de/timbolender/fefereader/FefeReaderApplication.kt
@@ -3,7 +3,6 @@ package de.timbolender.fefereader
 import android.app.Application
 import android.content.Intent
 import android.os.Environment
-import com.balsikandar.crashreporter.CrashReporter
 import com.facebook.stetho.Stetho
 import de.timbolender.fefereader.service.NotificationService
 
@@ -15,8 +14,6 @@ class FefeReaderApplication : Application() {
         super.onCreate()
 
         Stetho.initializeWithDefaults(this)
-
-        CrashReporter.initialize(this, Environment.getExternalStorageDirectory().absolutePath)
 
         val serviceIntent = Intent(this, NotificationService::class.java)
         startService(serviceIntent)
