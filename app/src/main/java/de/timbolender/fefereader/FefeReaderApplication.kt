fc54341735a41cc8a2a5191e55e838302b8827bc
Tim Bolender <git@timbolender.de>
2021-06-27 16:16:30 +0200

    Setup custom work manager configuration for increase logging

--- app/src/main/java/de/timbolender/fefereader/FefeReaderApplication.kt
+++ app/src/main/java/de/timbolender/fefereader/FefeReaderApplication.kt
@@ -2,12 +2,18 @@ package de.timbolender.fefereader
 
 import android.app.Application
 import android.content.Context
+import androidx.work.Configuration
 import com.facebook.stetho.Stetho
 
 /**
  * Basic set up.
  */
-class FefeReaderApplication : Application() {
+class FefeReaderApplication : Application(), Configuration.Provider {
+    override fun getWorkManagerConfiguration() =
+            Configuration.Builder()
+                    .setMinimumLoggingLevel(android.util.Log.DEBUG)
+                    .build()
+
     override fun onCreate() {
         super.onCreate()
 
