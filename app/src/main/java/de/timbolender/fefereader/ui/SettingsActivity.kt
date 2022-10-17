2444e3d364eacae3a061b2573990b680cd62bea9
Tim Bolender <git@timbolender.de>
2022-10-17 23:56:10 +0200

    Convert SettingsActivity to Kotlin

---
+++ app/src/main/java/de/timbolender/fefereader/ui/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package de.timbolender.fefereader.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import de.timbolender.fefereader.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
