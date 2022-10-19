eb4d382ddb909f50cc4ff3b79de410f4eb15fc8d
Tim Bolender <git@timbolender.de>
2022-10-19 23:09:11 +0200

    Make dark theme work in settings

--- app/src/main/java/de/timbolender/fefereader/ui/SettingsActivity.kt
+++ app/src/main/java/de/timbolender/fefereader/ui/SettingsActivity.kt
@@ -3,10 +3,18 @@ package de.timbolender.fefereader.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import de.timbolender.fefereader.R
+import de.timbolender.fefereader.ui.fragment.SettingsFragment
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_settings)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.mainFragment, SettingsFragment())
+            .commit()
     }
+
 }
